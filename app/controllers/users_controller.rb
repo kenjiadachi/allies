7a87c8851b1945f360d317f36551d3db3e068d38
“chisaki0606” <witness6961@gmail.com>
2020-10-10 15:50:59 +0900

    add users function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,31 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
 end
