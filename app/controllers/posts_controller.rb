ba44e360301b101c454d4ae140d6bef6d83d18ad
“chisaki0606” <witness6961@gmail.com>
2020-10-10 13:47:51 +0900

    add CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,21 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/")
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  get "posts/:id/destroy" => "posts#destroy"
 end
